ff6b27d2d33f74156d227fc30921e51a77c7335f
Avram Eisner <avrame@gmail.com>
2024-07-12 23:10:34 -0700

    Paddle using input methods instead of polling every frame

--- Scripts/paddle.gd
+++ Scripts/paddle.gd
@@ -8,8 +8,10 @@ var touching_right_wall = false
 var freeze_paddle = false
 
 @onready var sprite = $Sprite
-var paddle_width
-var screen_width
+var paddle_width: int
+var screen_width: int
+var move_amount: float
+var mouse_x_vel: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,23 +20,32 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-	
-func _physics_process(delta):
-	var mouse_x_vel = Input.get_last_mouse_velocity().x
 	if mouse_x_vel != 0:
 		if mouse_x_vel < 0 and position.x - paddle_width / 2 <= PADDLE_MARGIN:
 			position.x = paddle_width / 2
 		elif mouse_x_vel > 0 and position.x + paddle_width / 2 >= screen_width - PADDLE_MARGIN:
 			position.x = screen_width - paddle_width / 2
 		elif not freeze_paddle:
-			move_local_x((mouse_x_vel / 5) * delta)
+			move_local_x((mouse_x_vel / 3) * delta)
+		mouse_x_vel = 0
 	else:
-		var move_amount = Input.get_axis("slide-left", "slide-right")
 		var stop_move_left = move_amount < 0 and position.x - paddle_width / 2 <= PADDLE_MARGIN
 		var stop_move_right = move_amount > 0 and position.x + paddle_width / 2 >= screen_width - PADDLE_MARGIN
 		if not freeze_paddle and not stop_move_left and not stop_move_right:
 			move_local_x(move_amount * delta * PADDLE_SPEED)
+	
+func _physics_process(delta):
+	pass
+
+func _unhandled_input(event):
+	if event.is_action_pressed("slide-left") or event.is_action_pressed("slide-right"):
+		move_amount = Input.get_axis("slide-left", "slide-right")
+	elif event.is_action_released("slide-left") or event.is_action_released("slide-right"):
+		move_amount = 0
+		
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouse_x_vel = Input.get_last_mouse_velocity().x
 
 func _on_ball_entered(node):
 	if node.name == "Ball":
